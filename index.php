f382a45b36b6c00aaa9420cc3e9112422113d6ee
moradsimplon <morad.bzioui@laposte.net>
2017-09-06 12:08:22 +0200

    add forms with bootsnipp and add css

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+	<title>Material Tasarım Giriş ve Kayıt Formu</title>
+
+	<!-- Dışarıdan Çağırılan Dosyalar Font we Materyal İkonlar -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css" />
+
+	<!-- #Dışarıdan Çağırılan Dosyalar Font we Materyal İkonlar Bitiş -->
+
+
+</head>
+<body>
+
+<!-- Kapsayıcı -->
+<div class="col-lg-8 col-md-7 col-sm-6 col-xs-12     login-card">
+
+
+    <!-- Kayıt Form Sayfası -->
+    <form id="kayit-form" class="col-lg-12">
+
+        <div class="col-lg-12 logo-kapsul">
+            <img width="100" class="logo" src="https://selimdoyranli.com/cdn/material-form/img/logo.png" alt="Logo" />
+        </div>
+
+        <div style="clear:both;" class="col-lg-12"></div>
+
+
+        <a href="formulaire.php " class="kayit-ol-buton ">Sign In</a><br>
+        <a href="javascript:void(0);" class="kayit-ol-buton ">Sign Up</a>
+        <a class="zaten-hesap-var-link" href="">creer un compte , et venez vivre un super experience</a>
+
+
+
+</div>
+
+
+<script type="text/javascript" src='main.js'></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/js/mdb.min.js"></script>
+</body>
+</html>
